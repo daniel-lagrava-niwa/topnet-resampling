f3280aa490092a8ffd1d1dac7fafec6c78d0989e
lagravasandovald <daniel.lagravasandoval@niwa.co.nz>
2021-03-10 16:50:54 +1300

    Fixed an issue with the bash script to fix the streamq

--- bin/fix_streamq.sh
+++ bin/fix_streamq.sh
@@ -3,11 +3,11 @@
 usage="$0 streamq_file.nc"
 
 INPUT_FILE=$1
-if [ ${INPUT_FILE}=="" ]; then
+if [ "${INPUT_FILE}" == "" ]; then
   echo "Please provide an input file: ${usage}"
   exit 1
 fi
 
-ncatted -O -a bounds,time,d,, ${INPUT_FILE}
+ncatted -O -a bounds,time,d,, "${INPUT_FILE}"
 
 echo "Removed time:bounds from ${INPUT_FILE}"
